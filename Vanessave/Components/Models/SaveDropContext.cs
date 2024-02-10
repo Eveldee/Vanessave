7c60a94c969f2070cb52f8a69fa30c52e3ea1e18
Eveldee <eveldee0680@live.fr>
2024-02-10 16:42:04 +0100

    Add save and settings drag drop

---
+++ Vanessave/Components/Models/SaveDropContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Vanessave.Components.Models;
+
+public class SaveDropContext
+{
+    public event Func<IBrowserFile, Task>? OnFileDrop;
+
+    public Task DropFile(IBrowserFile browserFile)
+    {
+        return OnFileDrop?.Invoke(browserFile) ?? Task.CompletedTask;
+    }
+}
