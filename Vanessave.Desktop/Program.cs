89c3241caf9d9f57dada4bdd48ce6b0bbd6a7ba4
Eveldee <eveldee0680@live.fr>
2024-05-16 11:22:25 +0200

    Add Desktop app

---
+++ Vanessave.Desktop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+namespace Vanessave.Desktop;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+        appBuilder.Services
+            .AddLogging();
+
+        // Register root component
+        appBuilder.RootComponents.Add<App>("app");
+
+        var app = appBuilder.Build();
+
+        // Customize window
+        app.MainWindow
+            .SetIconFile("favicon.ico")
+            .SetTitle("Vanessave");
+
+        AppDomain.CurrentDomain.UnhandledException += (_, error) =>
+        {
+            app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+        };
+
+        app.Run();
+    }
+}
